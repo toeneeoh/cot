e93b2438d0413c3b6a57826331b65165a2e2be02
tony <toeneeoh@gmail.com>
2024-04-12 14:39:00 -0400

    moved functions / globals to root, optimized host detection

--- cotlua/src/config.lua
+++ cotlua/src/config.lua
@@ -1,49 +1,9 @@
-if Debug then Debug.beginFile 'Config' end
+--[[
+    config.lua
 
-function DetectHost()
-    local host = {id = -1, time = 0}
+    code that runs for players upon joining a lobby
+]]
 
-    for i = 0, PLAYER_CAP - 1 do
-        if (GetPlayerController(Player(i)) == MAP_CONTROL_USER and GetPlayerSlotState(Player(i)) == PLAYER_SLOT_STATE_PLAYING) then
-            print(User[Player(i)].nameColored .. " start time: " .. PLAYER_START_TIME[i] .. " | join time: " .. PLAYER_JOIN_TIME[i])
-            if PLAYER_START_TIME[i] - PLAYER_JOIN_TIME[i] > host.time then
-                host.time = PLAYER_START_TIME[i] - PLAYER_JOIN_TIME[i]
-                host.id = i
-            end
-        end
-    end
-
-    return host.id
+if LOCAL_JOIN_TIME == 0 then
+    LOCAL_JOIN_TIME = os.clock()
 end
-
-function OnStart()
-    local pid = GetPlayerId(GetTriggerPlayer())
-
-    PLAYER_START_TIME[pid] = tonumber(BlzGetTriggerSyncData())
-
-    return false
-end
-
-function OnJoin()
-    local pid = GetPlayerId(GetTriggerPlayer())
-
-    PLAYER_JOIN_TIME[pid] = tonumber(BlzGetTriggerSyncData())
-
-    return false
-end
-
-ON_JOIN = CreateTrigger()
-ON_START = CreateTrigger()
-PLAYER_JOIN_TIME = __jarray(0)
-PLAYER_START_TIME = __jarray(0)
-
-for i = 0, bj_MAX_PLAYER_SLOTS do
-    BlzTriggerRegisterPlayerSyncEvent(ON_JOIN, Player(i), "join", false)
-    BlzTriggerRegisterPlayerSyncEvent(ON_START, Player(i), "start", false)
-end
-TriggerAddCondition(ON_JOIN, Condition(OnJoin))
-TriggerAddCondition(ON_START, Condition(OnStart))
-
-BlzSendSyncData("join", tostring(os.clock()))
-
-if Debug then Debug.endFile() end
