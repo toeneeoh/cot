3b2da422d9dc3051ccc7a85c89692bd08f3e23ed
tony <toeneeoh@gmail.com>
2024-04-12 15:35:11 -0400

    use with build tool

--- cotlua/src/root.lua
+++ cotlua/src/root.lua
@@ -1,3 +1,115 @@
+--[[
+    root.lua
+
+    defines some helper functions and then indicates what files the build tool should load into the map.
+    (file dependencies determined by TotalInitialization.lua)
+]]
+
+do
+    LOCAL_JOIN_TIME = 0
+    PLAYER_JOIN_TIME = __jarray(0)
+    PLAYER_START_TIME = __jarray(0)
+
+    --functions to determine which player is the host based on lobby join time
+    function OnStart()
+        PLAYER_START_TIME[GetTriggerPlayer()] = tonumber(BlzGetTriggerSyncData())
+    end
+
+    function OnJoin()
+        PLAYER_JOIN_TIME[GetTriggerPlayer()] = tonumber(BlzGetTriggerSyncData())
+    end
+
+    function DetectHost()
+        local host = {p = nil, time = 0}
+
+        for i = 0, bj_MAX_PLAYERS do
+            local p = Player(i)
+
+            if (GetPlayerController(p) == MAP_CONTROL_USER and GetPlayerSlotState(p) == PLAYER_SLOT_STATE_PLAYING) then
+                print(User[p].nameColored .. " start time: " .. PLAYER_START_TIME[p] .. " | join time: " .. PLAYER_JOIN_TIME[p])
+                if PLAYER_START_TIME[p] - PLAYER_JOIN_TIME[p] > host.time then
+                    host.time = PLAYER_START_TIME[p] - PLAYER_JOIN_TIME[p]
+                    host.p = p
+                end
+            end
+        end
+
+        return host.p
+    end
+
+    --iterator for groups with counter
+    ---@param ug group
+    ---@return fun(): integer?, unit?
+    function ieach(ug)
+        local index = -1
+        local length = BlzGroupGetSize(ug)
+
+        return function()
+            index = index + 1
+            if index < length then
+                return index, BlzGroupUnitAt(ug, index)
+            end
+        end
+    end
+
+    --iterator for groups
+    ---@param ug group
+    ---@return fun(): unit?
+    function each(ug)
+        local index = -1
+        local length = BlzGroupGetSize(ug)
+
+        return function()
+            index = index + 1
+            if index < length then
+                return BlzGroupUnitAt(ug, index)
+            end
+        end
+    end
+
+    --credits: Bribe
+    local mts = {}
+    local weakKeys = {__mode="k"} --ensures tables with non-nilled objects as keys will be garbage collected.
+
+    ---Re-define __jarray.
+    ---@param default? any
+    ---@param tab? table
+    ---@return table
+    __jarray = function(default, tab)
+        local mt
+        if default then
+            mts[default]=mts[default] or {
+                __index=function()
+                    return default
+                end,
+                __mode="k"
+            }
+            mt=mts[default]
+        else
+            mt=weakKeys
+        end
+        return setmetatable(tab or {}, mt)
+    end
+
+    local nested_mts = {}
+
+    --returns a 2d array with a default value
+    ---@type fun(val: any): table
+    function array2d(val)
+        local key = val or "___"
+
+        nested_mts[key] = nested_mts[key] or {
+            __index = function(t, k)
+                local new = (val and __jarray(val)) or {}
+
+                rawset(t, k, new)
+                return new
+            end}
+
+        return setmetatable({}, nested_mts[key])
+    end
+end
+
 dofile('debugutils.lua')
 dofile('stringwidth.lua')
 dofile('ingameconsole.lua')
@@ -21,7 +133,7 @@ dofile('codegen.lua')
 dofile('unitevent.lua')
 dofile('spellview.lua')
 dofile('pathing.lua')
-dofile('pathfinding.lua')
+--dofile('pathfinding.lua')
 dofile('unittable.lua')
 dofile('dummy.lua')
 dofile('playerdata.lua')
@@ -57,3 +169,4 @@ dofile('summon.lua')
 dofile('level.lua')
 dofile('currency.lua')
 dofile('train.lua')
+dofile('movespeed.lua')
