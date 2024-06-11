f5e282130889a4488c7e06d57eddf5c15afd0a55
toeneeoh <toeneeoh@gmail.com>
2024-06-11 19:41:51 -0400

    big refactor for fun

--- cotlua/src/codegen.lua
+++ cotlua/src/codegen.lua
@@ -8,15 +8,12 @@
 ]]
 
 OnInit.global("CodeGen", function()
-
-    SaveData = {} ---@type table
-
     local ALPHABET        = "!#$\x25&'()*+,-.0123456789:;=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}`" ---@type string 
-    local BASE ---@type integer 
-    local CHAR=__jarray("") ---@type string[] 
-    local MAXVALUE         = 7 ---@type integer 
-    local VALID ---@type boolean 
-    local ERROR        = "" ---@type string 
+    local ERROR           = ""
+    local MAX_SPACE       = 6
+    local BASE            = ALPHABET:len() - MAX_SPACE - 1
+    local CHAR            = ALPHABET:sub(2, MAX_SPACE + 1)
+    ALPHABET              = ALPHABET:sub(1, 1) .. ALPHABET:sub(MAX_SPACE + 2)
 
     ---@return string
     function GetCodeGenError()
@@ -26,16 +23,15 @@ OnInit.global("CodeGen", function()
     ---@param i integer
     ---@return string
     function Encode(i)
-        local b ---@type integer 
-        local s = "" ---@type string 
+        local s = ""
 
         if i < BASE then
-            return SubString(ALPHABET, i, i + 1)
+            return ALPHABET:sub(i + 1, i + 1)
         end
 
         while i > 0 do
-            b = i - (i // BASE) * BASE
-            s = SubString(ALPHABET, b, b + 1) .. s
+            local b = i - (i // BASE) * BASE
+            s = ALPHABET:sub(b + 1, b + 1) .. s
             i = i // BASE
         end
 
@@ -45,13 +41,12 @@ OnInit.global("CodeGen", function()
     ---@param s string
     ---@return integer
     local function StrPos(s)
-        local i = 0 ---@type integer 
-        while i < BASE do
-            if s == SubString(ALPHABET, i, i + 1) then
-                return i
-            end
-            i = i + 1
+        local pos = ALPHABET:find(s, 1, true)
+
+        if pos then
+            return pos - 1
         end
+
         return -1
     end
 
@@ -60,9 +55,9 @@ OnInit.global("CodeGen", function()
     function Decode(s)
         local a = 0 ---@type integer 
 
-        while not (StringLength(s) == 1) do
-            a = a * BASE + BASE * StrPos(SubString(s, 0, 1))
-            s = SubString(s, 1, 99)
+        while s:len() ~= 1 do
+            a = a * BASE + BASE * StrPos(s:sub(1, 1))
+            s = s:sub(2)
         end
 
         return a + StrPos(s)
@@ -74,107 +69,86 @@ OnInit.global("CodeGen", function()
         local o = 0 ---@type integer 
 
         for i = 1, in_:len() do
-            o = o + Decode(in_:sub(i, i + 1))
+            o = o + Decode(in_:sub(i, i))
         end
 
         return o
     end
 
-    -- yeeahh descriptive variables
-    ---@type fun(str: string, p: player): boolean
+    ---@type fun(str: string, p: player): boolean|table
     function Load(str, p)
-        local pid = GetPlayerId(p) + 1 ---@type integer 
-
-        VALID = false
+        local VALID = false
 
         for i = 1, 3 do
-            if (Decode(SubString(str, 0, i)) == StringChecksum(SubString(str, i, 999))) then
+            if DEV_ENABLED then
+                print("Decode checksum: " .. Decode(str:sub(1, i)) .. " " .. str:sub(1, i))
+                print("Checksum " .. i .. ": " .. StringChecksum(str:sub(i + 1)) .. " " .. str:sub(i + 1))
+            end
+
+            if Decode(str:sub(1, i)) == StringChecksum(str:sub(i + 1)) then
                 VALID = true
-                str = SubString(str, i, 999)
+                str = str:sub(i + 1)
                 break
             end
         end
 
-        if (not VALID) then
+        local checksum = Encode(StringChecksum(GetPlayerName(p)))
+        local checksum_len = checksum:len()
+
+        if (not VALID) or checksum ~= str:sub(-checksum_len) then
             ERROR = "Invalid code"
-            return VALID
+            return false
         end
 
-        local l = StringLength(str)
-        local c = Encode(StringChecksum(GetPlayerName(p)))
-        local i = StringLength(c)
-
-        if (c ~= SubString(str, l - i, i)) then
-            VALID = false
-            ERROR = "Invalid code"
-            return VALID
-        end
-
-        l = l - i
-        i = 0
+        str = str:sub(1, -checksum_len - 1)
         local data = {}
+        i = 1
 
-        while i < l do
-            local tmp = SubString(str, i, i + 1)
-            local b = true
-            local f = 0
-            local j = 1
+        while i <= #str do
+            local tmp = str:sub(i, i)
+            local token_length = (CHAR:find(tmp) or 0) + 1
 
-            while not (f >= (MAXVALUE)) do
-                if (tmp == CHAR[f]) then
-                    j = f + 2
-                    data[#data + 1] = Decode(SubString(str, i + 1, i + (j)))
-                    b = false
-                    f = MAXVALUE
-                end
-                f = f + 1
+            --ignore space indicator
+            if token_length > 1 then
+                i = i + 1
             end
 
-            if (b) then
-                data[#data + 1] = Decode(tmp)
-            end
-
-            i = i + j
+            local token = str:sub(i, i + token_length - 1)
+            data[#data + 1] = Decode(token)
+            i = i + token_length
         end
 
-        SaveData[pid] = data
-        VALID = true
-
-        return VALID
+        return data
     end
 
     ---@type fun(pid: integer, data: table): string
     function Compile(pid, data)
-        local j   = 0 ---@type integer 
         local out = "" ---@type string 
-        local x   = "" ---@type string 
         local p   = Player(pid - 1) ---@type player 
 
         for _, v in ipairs(data) do
-            x = Encode(v)
-            j = StringLength(x)
+            local x = Encode(v)
+            local j = x:len()
 
             if (j > 1) then
-                out = out .. CHAR[j - 1]
+                out = out .. CHAR:sub(j - 1, j - 1)
             end
 
             out = out .. x
         end
 
+        --appends player name checksum to end of string
         out = out .. Encode(StringChecksum(GetPlayerName(p)))
 
-        out = Encode(StringChecksum(out)) .. out
+        --appends total checksum to beginning of string
+        local cs = StringChecksum(out)
+        out = Encode(cs) .. out
+
+        if DEV_ENABLED then
+            print("Checksum: " .. cs .. " Encoded: " .. Encode(cs))
+        end
 
         return out
     end
 
-        local b = StringLength(ALPHABET) ---@type integer 
-        local m = MAXVALUE ---@type integer 
-
-        for i = 1, MAXVALUE - 1 do
-            CHAR[i] = SubString(ALPHABET, i, i + 1)
-        end
-
-        ALPHABET = SubString(ALPHABET, 0, 1) .. SubString(ALPHABET, m + 1, b)
-        BASE = b - m
 end, Debug.getLine())
