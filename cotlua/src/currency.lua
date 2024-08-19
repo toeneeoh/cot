9d69df67d622d8306d964d9f26b530515ff4d1a6
toeneeoh <toeneeoh@gmail.com>
2024-08-19 18:42:35 -0400

    remove arcadite references

--- cotlua/src/currency.lua
+++ cotlua/src/currency.lua
@@ -10,7 +10,6 @@ OnInit.final("Currency", function(Require)
     Require('Items')
 
     IS_CONVERTING_PLAT = {} ---@type boolean[]
-    IS_CONVERTING_ARC = {} ---@type boolean[]
     IS_CONVERTER_PURCHASED = {} ---@type boolean[]
 
     -- purchase auto converter
@@ -21,13 +20,11 @@ OnInit.final("Currency", function(Require)
 
         -- converter
         if index == 0 then
-            if GetCurrency(pid, PLATINUM) >= 2 and GetCurrency(pid, ARCADITE) >= 2 then
+            if GetCurrency(pid, PLATINUM) >= 4 then
                 IS_CONVERTER_PURCHASED[pid] = true
-                AddCurrency(pid, PLATINUM, -2)
-                AddCurrency(pid, ARCADITE, -2)
+                AddCurrency(pid, PLATINUM, -4)
                 if GetLocalPlayer() == GetTriggerPlayer() then
                     PLAT_CONVERT_FRAME.tooltip:text("Convert gold to platinum automatically")
-                    ARC_CONVERT_FRAME.tooltip:text("Convert lumber to arcadite automatically")
                 end
             else
                 DisplayTimedTextToPlayer(Player(pid - 1), 0, 0, 10, "You cannot afford this!")
@@ -42,7 +39,7 @@ OnInit.final("Currency", function(Require)
     -- map item currency exchange purchases to functions
     ITEM_LOOKUP[FourCC('I084')] = function(p, pid)
         if not IS_CONVERTER_PURCHASED[pid] then
-            local dw = DialogWindow.create(pid, "Purchase cost: |n|cffffffff2 |cffe3e2e2Platinum|r|nand |cffffffff2 |cff66FF66Arcadite|r", BuyConverter)
+            local dw = DialogWindow.create(pid, "Purchase cost: |n|cffffffff4 |cffe3e2e2Platinum|r", BuyConverter)
 
             dw:addButton("Purchase")
 
@@ -65,7 +62,7 @@ OnInit.final("Currency", function(Require)
         if GetCurrency(pid, PLATINUM) >= 3 then
             AddCurrency(pid, PLATINUM, -3)
             AddCurrency(pid, CRYSTAL, 1)
-            DisplayTimedTextToPlayer(p, 0, 0, 20, CrystalTag + (GetCurrency(pid, CRYSTAL)))
+            DisplayTimedTextToPlayer(p, 0, 0, 20, CrystalTag .. (GetCurrency(pid, CRYSTAL)))
         else
             DisplayTimedTextToPlayer(p, 0, 0, 20, "You need at least 3 platinum to buy this.")
         end
@@ -82,66 +79,6 @@ OnInit.final("Currency", function(Require)
         end
     end
 
-    ITEM_LOOKUP[FourCC('I04H')] = function(p, pid)
-        if GetCurrency(pid, LUMBER) >= 1000000 then
-            AddCurrency(pid, ARCADITE, 1)
-            AddCurrency(pid, LUMBER, -1000000)
-            ConversionEffect(pid)
-            DisplayTimedTextToPlayer(p, 0, 0, 20, ArcTag .. (GetCurrency(pid, ARCADITE)))
-        else
-            DisplayTimedTextToPlayer(p, 0, 0, 30, "|cffee0000You do not have a million lumber to convert.")
-        end
-    end
-
-    ITEM_LOOKUP[FourCC('I054')] = function(p, pid)
-        if GetCurrency(pid, ARCADITE) >0 then
-            ConversionEffect(pid)
-            AddCurrency(pid, PLATINUM, 1)
-            AddCurrency(pid, ARCADITE, -1)
-            AddCurrency(pid, GOLD, 200000)
-            DisplayTimedTextToPlayer(p, 0, 0, 20, ArcTag .. (GetCurrency(pid, ARCADITE)))
-            DisplayTimedTextToPlayer(p, 0, 0, 20, PlatTag .. (GetCurrency(pid, PLATINUM)))
-        else
-            DisplayTimedTextToPlayer(p, 0, 0, 20, "|cff990000Unable to convert; not enough Arcadite Lumber.")
-        end
-    end
-
-    ITEM_LOOKUP[FourCC('I053')] = function(p, pid)
-        if GetCurrency(pid, PLATINUM) > 0 then
-            ConversionEffect(pid)
-            AddCurrency(pid, PLATINUM, -1)
-            AddCurrency(pid, ARCADITE, 1)
-            DisplayTimedTextToPlayer(p, 0, 0, 20, ArcTag .. (GetCurrency(pid, ARCADITE)))
-            DisplayTimedTextToPlayer(p, 0, 0, 20, PlatTag .. (GetCurrency(pid, PLATINUM)))
-        else
-            AddCurrency(pid, GOLD, 350000)
-            DisplayTimedTextToPlayer(p, 0, 0, 20, "|cff990000Unable to convert; not enough Platinum Coins.")
-        end
-    end
-
-    ITEM_LOOKUP[FourCC('I0PA')] = function(p, pid)
-        if GetCurrency(pid, PLATINUM) >= 4 then
-            ConversionEffect(pid)
-            AddCurrency(pid, PLATINUM, -4)
-            AddCurrency(pid, ARCADITE, 3)
-            DisplayTimedTextToPlayer(p, 0, 0, 20, ArcTag .. (GetCurrency(pid, ARCADITE)))
-            DisplayTimedTextToPlayer(p, 0, 0, 20, PlatTag .. (GetCurrency(pid, PLATINUM)))
-        else
-            DisplayTimedTextToPlayer(p, 0, 0, 20, "|cff990000Unable to convert; not enough Platinum Coins.")
-        end
-    end
-
-    ITEM_LOOKUP[FourCC('I051')] = function(p, pid)
-        if GetCurrency(pid, ARCADITE) > 0 then
-            ConversionEffect(pid)
-            AddCurrency(pid, ARCADITE, -1)
-            AddCurrency(pid, LUMBER, 1000000)
-            DisplayTimedTextToPlayer(p, 0, 0, 20, ArcTag .. (GetCurrency(pid, ARCADITE)))
-        else
-            DisplayTimedTextToPlayer(p, 0, 0, 20, "|cff990000Unable to convert; not enough Arcadite Lumber.")
-        end
-    end
-
     ITEM_LOOKUP[FourCC('I052')] = function(p, pid)
         if GetCurrency(pid, PLATINUM) >0 then
             ConversionEffect(pid)
@@ -153,42 +90,14 @@ OnInit.final("Currency", function(Require)
         end
     end
 
-    ITEM_LOOKUP[FourCC('I03R')] = function(p, pid)
-        if GetCurrency(pid, LUMBER) >= 25000 then
-            AddCurrency(pid, GOLD, 25000)
-            AddCurrency(pid, LUMBER, -25000)
-            DestroyEffect(AddSpecialEffectTarget("Abilities\\Spells\\Items\\ResourceItems\\ResourceEffectTarget.mdl", Hero[pid], "origin"))
-        else
-            DisplayTimedTextToPlayer(p, 0, 0, 20, "You need at least 25,000 lumber to buy this.")
-        end
-    end
-
-    ITEM_LOOKUP[FourCC('I05C')] = function(p, pid)
-        if GetCurrency(pid, GOLD) >= 32000 then
-            AddCurrency(pid, LUMBER, 25000)
-            AddCurrency(pid, GOLD, -32000)
-            DestroyEffect(AddSpecialEffectTarget("Abilities\\Spells\\Items\\ResourceItems\\ResourceEffectTarget.mdl", Hero[pid], "origin"))
-        else
-            DisplayTimedTextToPlayer(p, 0, 0, 20, "You need at least 32,000 gold to buy this.")
-        end
-    end
-
     ---@type fun(pid: integer, index: integer, amount: integer)
     function SetCurrency(pid, index, amount)
         Currency[pid * CURRENCY_COUNT + index] = IMaxBJ(0, amount)
 
         if index == GOLD then
             SetPlayerState(Player(pid - 1), PLAYER_STATE_RESOURCE_GOLD, Currency[pid * CURRENCY_COUNT + index])
-        elseif index == LUMBER then
-            SetPlayerState(Player(pid - 1), PLAYER_STATE_RESOURCE_LUMBER, Currency[pid * CURRENCY_COUNT + index])
         elseif index == PLATINUM then
-            if GetLocalPlayer() == Player(pid - 1) then
-                BlzFrameSetText(PLAT_TEXT, tostring(Currency[pid * CURRENCY_COUNT + index]))
-            end
-        elseif index == ARCADITE then
-            if GetLocalPlayer() == Player(pid - 1) then
-                BlzFrameSetText(ARC_TEXT, tostring(Currency[pid * CURRENCY_COUNT + index]))
-            end
+            SetPlayerState(Player(pid - 1), PLAYER_STATE_RESOURCE_LUMBER, Currency[pid * CURRENCY_COUNT + index])
         elseif index == CRYSTAL then
             SetPlayerState(Player(pid - 1), PLAYER_STATE_RESOURCE_FOOD_USED, Currency[pid * CURRENCY_COUNT + index])
         end
@@ -238,7 +147,7 @@ OnInit.final("Currency", function(Require)
             s = "|cffcccccc+" .. platWon .. "|r |cffffcc00" .. s .. "|r"
             FloatingTextUnit(s, Hero[pid], 1.5, 75, -100, 9., 255, 255, 255, 0, false)
         else
-            FloatingTextUnit(s, Hero[pid], 1.5, 75, -100, 8.5, 255, 255, 0, 0, false)
+            FloatingTextUnit(s, Hero[pid], 1.5, 75, -100, 9., 255, 255, 0, 0, false)
         end
     end
 
@@ -246,9 +155,7 @@ OnInit.final("Currency", function(Require)
     function ChargeNetworth(p, flat, percent, minimum, message)
         local pid          = GetPlayerId(p) + 1
         local playerGold   = GetCurrency(pid, GOLD)
-        local playerLumber = GetCurrency(pid, LUMBER)
         local platCost     = R2I(GetCurrency(pid, PLATINUM) * percent)
-        local arcCost      = R2I(GetCurrency(pid, ARCADITE) * percent)
 
         local cost = flat + R2I(playerGold * percent)
         if cost < minimum then
@@ -266,20 +173,7 @@ OnInit.final("Currency", function(Require)
             end
         end
 
-        cost = flat + R2I(playerLumber * percent)
-        if cost < minimum then
-            cost = minimum
-        end
-
-        AddCurrency(pid, LUMBER, -cost)
-        AddCurrency(pid, ARCADITE, -arcCost)
-
         if message ~= "" then
-            if arcCost > 0 then
-                message = message .. ", " .. RealToString(arcCost) .. " arcadite, and " .. RealToString(cost) .. " lumber."
-            else
-                message = message .. " and " .. RealToString(cost) .. " lumber."
-            end
             DisplayTextToPlayer(p, 0, 0, message)
         end
     end
@@ -290,7 +184,6 @@ OnInit.final("Currency", function(Require)
         local pid = GetPlayerId(p) + 1 ---@type integer 
 
         Currency[pid * CURRENCY_COUNT + GOLD] = GetPlayerState(p, PLAYER_STATE_RESOURCE_GOLD)
-        Currency[pid * CURRENCY_COUNT + LUMBER] = GetPlayerState(p, PLAYER_STATE_RESOURCE_LUMBER)
 
         if IS_CONVERTING_PLAT[pid] then
             local plat = Currency[pid * CURRENCY_COUNT + GOLD] // 1000000
@@ -298,16 +191,6 @@ OnInit.final("Currency", function(Require)
             if plat > 0 then
                 AddCurrency(pid, PLATINUM, plat)
                 AddCurrency(pid, GOLD, -1000000 * plat)
-                ConversionEffect(pid)
-            end
-        end
-
-        if IS_CONVERTING_ARC[pid] then
-            local arc = Currency[pid * CURRENCY_COUNT + LUMBER] // 1000000
-
-            if arc > 0 then
-                AddCurrency(pid, ARCADITE, arc)
-                AddCurrency(pid, LUMBER, -1000000 * arc)
                 ConversionEffect(pid)
             end
         end
