98ff195e7cfdd46420343c8f9aa1535d845bedb7
toeneeoh <toeneeoh@gmail.com>
2024-08-19 19:17:04 -0400

    update stats on level, remove base xp warning

--- cotlua/src/Events/level.lua
+++ cotlua/src/Events/level.lua
@@ -7,6 +7,16 @@
 OnInit.final("Level", function(Require)
     Require('Users')
 
+    local old_set_level = SetHeroLevel
+    SetHeroLevel = function(u, lvl, eye_candy)
+        old_set_level(u, lvl, eye_candy)
+        Unit[u].str = Unit[u].str
+        Unit[u].agi = Unit[u].agi
+        Unit[u].int = Unit[u].int
+        SetWidgetLife(u, BlzGetUnitMaxHP(u))
+        SetUnitState(u, UNIT_STATE_MANA, BlzGetUnitMaxMana(u))
+    end
+
     local function OnLevel()
         local u     = GetTriggerUnit() ---@type unit 
         local p     = GetOwningPlayer(u)
@@ -15,42 +25,32 @@ OnInit.final("Level", function(Require)
         local uid   = GetUnitTypeId(u) ---@type integer 
 
         if u == Hero[pid] then
-            if uid == HERO_DARK_SUMMONER then --summoning improvement level
+            if uid == HERO_DARK_SUMMONER then -- summoning improvement level
                 SetUnitAbilityLevel(u, SUMMONINGIMPROVEMENT.id, level // 10 + 1)
-            elseif uid == HERO_DARK_SAVIOR then --dark seal level
+            elseif uid == HERO_DARK_SAVIOR then -- dark seal level
                 SetUnitAbilityLevel(u, DARKSEAL.id, level // 100 + 1)
-            elseif uid == HERO_SAVIOR then --light seal level
+            elseif uid == HERO_SAVIOR then -- light seal level
                 SetUnitAbilityLevel(u, LIGHTSEAL.id, level // 100 + 1)
-            elseif uid == HERO_OBLIVION_GUARD then --body of fire level
+            elseif uid == HERO_OBLIVION_GUARD then -- body of fire level
                 SetUnitAbilityLevel(u, BODYOFFIRE.id, level // 100 + 1)
-            --elseif uid == HERO_MARKSMAN or uid == HERO_MARKSMAN_SNIPER then --sniper stance level
-                --SetUnitAbilityLevel(u, SNIPERSTANCE.id, level // 50 + 1)
-            elseif uid == HERO_THUNDERBLADE then --overload level
+            elseif uid == HERO_PHOENIX_RANGER then -- multishot level
+                SetUnitAbilityLevel(u, FourCC('A05R'), math.min(level // 50 + 1, 5))
+            elseif uid == HERO_THUNDERBLADE then -- overload level
                 SetUnitAbilityLevel(u, OVERLOAD.id, level // 75 + 1)
-            elseif uid == HERO_ASSASSIN then --blade spin level
+            elseif uid == HERO_ASSASSIN then -- blade spin level
                 SetUnitAbilityLevel(u, BLADESPIN.id, IMinBJ(4, level // 100 + 1))
                 SetUnitAbilityLevel(u, BLADESPIN.id2, IMinBJ(4, level // 100 + 1))
-            elseif uid == HERO_MASTER_ROGUE then --instant death level
+            elseif uid == HERO_MASTER_ROGUE then -- instant death level
                 SetUnitAbilityLevel(u, INSTANTDEATH.id, level // 50 + 1)
                 INSTANTDEATH.apply(u, pid)
             end
 
-            if not LOAD_FLAG[pid] then
-                if level >= 180 and Profile[pid].hero.base <= 4 then
-                    SoundHandler("Sound\\Interface\\SecretFound.wav", false, p)
-                    DisplayTimedTextToPlayer(p, 0, 0, 60., "|cffff0000You have reached level 180 and no longer earn experience with regular homes, you must purchase a chaotic home!|r")
-                elseif level <= 15 and Profile[pid].hero.base == 0 then
-                    SoundHandler("Sound\\Interface\\SecretFound.wav", false, p)
-                    DisplayTimedTextToPlayer(p, 0, 0, 60., "You will stop gaining experience after level 15 without a home, purchase one from the vendors in town and build it near a gold mine.")
-                end
-            end
-
             SuspendHeroXP(Backpack[pid], false)
-            SetHeroLevel(Backpack[pid],GetHeroLevel(Hero[pid]),false)
+            SetHeroLevel(Backpack[pid], GetHeroLevel(Hero[pid]),false)
             SuspendHeroXP(Backpack[pid], true)
 
-            --update restricted items
-            for i = 6, MAX_INVENTORY_SLOTS - 1 do
+            -- update restricted items
+            for i = 7, MAX_INVENTORY_SLOTS do
                 local itm = Profile[pid].hero.items[i]
 
                 if itm then
@@ -60,7 +60,12 @@ OnInit.final("Level", function(Require)
                 end
             end
 
-            --trigger stat change event
+            -- update stats TODO: remove built in stat gain?
+            Unit[u].str = Unit[u].str
+            Unit[u].agi = Unit[u].agi
+            Unit[u].int = Unit[u].int
+
+            -- trigger stat change event
             EVENT_STAT_CHANGE:trigger(u)
 
             ExperienceControl(pid)
