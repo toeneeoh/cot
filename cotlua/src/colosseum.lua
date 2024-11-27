e8986abce078b646f5050c083f6eb3e065d035bb
toeneeoh <toeneeoh@gmail.com>
2024-11-27 09:52:14 -0500

    TODO

---
+++ cotlua/src/colosseum.lua
@@ -0,0 +1,7 @@
+OnInit.final("Colosseum", function(Require)
+    ITEM_LOOKUP[FourCC('I0ER')] = function(p, pid)
+
+        -- TODO: Check for ticket, prompt all users via custom frame to accept or deny joining colosseum
+        -- Difficulty will scale by player level and relative strength (?)
+    end
+end, Debug and Debug.getLine())
