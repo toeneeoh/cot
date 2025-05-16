db2d90fd43eaa1ef63b9948c969a6750ac6ba5f8
toeneeoh <toeneeoh@gmail.com>
2025-05-15 22:38:04 -0400

    remove afk frames, move currency conversion frames, remove old talent frames

--- cotlua/src/UI/frames.lua
+++ cotlua/src/UI/frames.lua
@@ -116,33 +116,8 @@ OnInit.final("Frames", function(Require)
         BlzFrameSetAbsPoint(FH, FRAMEPOINT_TOPRIGHT, 0.5885, 0.5965)
         BlzFrameSetTextColor(FH, BlzConvertColor(255, 108, 160, 176)) -- Crystal Blue
 
-        local CONVERTER_FRAME = BlzCreateFrame("QuestButtonDisabledBackdropTemplate", RESOURCE_BAR, 0, 0)
-        BlzFrameSetTexture(CONVERTER_FRAME, "trans32.blp", 0, true)
-        BlzFrameSetSize(CONVERTER_FRAME, 0.006, 0.006)
-        BlzFrameSetPoint(CONVERTER_FRAME, FRAMEPOINT_TOP, RESOURCE_BAR, FRAMEPOINT_TOP, 0, -0.025)
-
-        -- converter buttons
-        local onConvert = function()
-            local frame = Button.table[BlzGetTriggerFrame()]
-            local pid = GetPlayerId(GetTriggerPlayer()) + 1
-            local enabled = false
-
-            if IS_CONVERTER_PURCHASED[pid] then
-                if frame == PLAT_CONVERT_FRAME then
-                    enabled = not IS_CONVERTING_PLAT[pid]
-                    IS_CONVERTING_PLAT[pid] = enabled
-                end
-            end
-
-            if GetLocalPlayer() == Player(pid - 1) then
-                BlzFrameSetEnable(frame.frame, false)
-                BlzFrameSetEnable(frame.frame, true)
-                frame:enabled(enabled)
-            end
-        end
-
         -- Honor (Custom)
-        HONOR_TEXT = BlzCreateFrame("CurrencyText", CONVERTER_FRAME, 0, 0)
+        HONOR_TEXT = BlzCreateFrame("CurrencyText", RESOURCE_BAR, 0, 0)
         BlzFrameSetAbsPoint(HONOR_TEXT, FRAMEPOINT_CENTER, 0.355, 0.5690)
         BlzFrameSetScale(HONOR_TEXT, 1.0)
         BlzFrameSetTextAlignment(HONOR_TEXT, TEXT_JUSTIFY_MIDDLE, TEXT_JUSTIFY_RIGHT)
@@ -150,33 +125,27 @@ OnInit.final("Frames", function(Require)
         BlzFrameSetTextColor(HONOR_TEXT, BlzConvertColor(255, 255, 135, 141)) -- Tulip
 
         -- Faction (Custom)
-        FACTION_TEXT = BlzCreateFrame("CurrencyText", CONVERTER_FRAME, 0, 0)
+        FACTION_TEXT = BlzCreateFrame("CurrencyText", RESOURCE_BAR, 0, 0)
         BlzFrameSetAbsPoint(FACTION_TEXT, FRAMEPOINT_CENTER, 0.455, 0.5690)
         BlzFrameSetScale(FACTION_TEXT, 1.0)
         BlzFrameSetText(FACTION_TEXT, "0")
         BlzFrameSetTextColor(FACTION_TEXT, BlzConvertColor(255, 76, 145, 65)) -- May Green
 
-        PLAT_CONVERT_FRAME = Button.create(CONVERTER_FRAME, 0.018, 0.018, -0.105, 0, true)
-        PLAT_CONVERT_FRAME:icon("ReplaceableTextures\\CommandButtons\\BTNConvert.blp")
-        PLAT_CONVERT_FRAME:onClick(onConvert)
-        PLAT_CONVERT_FRAME:enabled(false)
-        PLAT_CONVERT_FRAME.tooltip:text("Must purchase a converter to use!")
-
-        HONOR_FRAME = BlzCreateFrameByType("FRAME", "", CONVERTER_FRAME, "", 0)
+        HONOR_FRAME = BlzCreateFrameByType("FRAME", "", RESOURCE_BAR, "", 0)
         BlzFrameSetTexture(HONOR_FRAME, "trans32.blp", 0, true)
         BlzFrameSetPoint(HONOR_FRAME, FRAMEPOINT_TOP, RESOURCE_BAR, FRAMEPOINT_TOP, - 0.055, - 0.024)
         BlzFrameSetSize(HONOR_FRAME, 0.062, 0.02)
         BlzFrameSetLevel(HONOR_FRAME, 1)
 
-        FrameAddSimpleTooltip(HONOR_FRAME, "Honor", "Umm", false)
+        FrameAddSimpleTooltip(HONOR_FRAME, "Honor", "Obtained by emerging victorious at the Colosseum.", false)
 
-        FACTION_FRAME = BlzCreateFrameByType("FRAME", "", CONVERTER_FRAME, "", 0)
+        FACTION_FRAME = BlzCreateFrameByType("FRAME", "", RESOURCE_BAR, "", 0)
         BlzFrameSetTexture(FACTION_FRAME, "trans32.blp", 0, true)
         BlzFrameSetPoint(FACTION_FRAME, FRAMEPOINT_TOP, RESOURCE_BAR, FRAMEPOINT_TOP, 0.055, - 0.024)
         BlzFrameSetSize(FACTION_FRAME, 0.062, 0.02)
         BlzFrameSetLevel(FACTION_FRAME, 1)
 
-        FrameAddSimpleTooltip(FACTION_FRAME, "Faction Points", "Umm", false)
+        FrameAddSimpleTooltip(FACTION_FRAME, "Faction Points", "Obtained by completing Faction Quests and may be spent at Faction Shops.", false)
 
         -- Upkeep // HIDDEN
         FH = BlzGetFrameByName("ResourceBarUpkeepText", 0)
@@ -283,113 +252,6 @@ OnInit.final("Frames", function(Require)
         end)
     end
 
-    --[[ talent tree
-    do
-        local leftTreeAlignment   = 0.07 ---@type number 
-        local middleTreeAlignment = 0.31 ---@type number 
-        local rightTreeAlignment  = 0.5 ---@type number 
-        local treeSize            = 0.23 ---@type number 
-        local smallTreeSize       = 0.18 ---@type number 
-
-        TalentMainFrame = BlzCreateFrame("EscMenuBackdrop", BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0), 0, 0)
-        BlzFrameSetAbsPoint(TalentMainFrame, FRAMEPOINT_TOPLEFT, 0.05, 0.56)
-        BlzFrameSetAbsPoint(TalentMainFrame, FRAMEPOINT_BOTTOMRIGHT, 0.75, 0.152300)
-        BlzFrameSetVisible(TalentMainFrame, false)
-
-        TreeLeft = BlzCreateFrame("ListBoxWar3", TalentMainFrame, 0, 1)
-        BlzFrameSetAbsPoint(TreeLeft, FRAMEPOINT_TOPLEFT, leftTreeAlignment, 0.52)
-        BlzFrameSetAbsPoint(TreeLeft, FRAMEPOINT_BOTTOMRIGHT, leftTreeAlignment + treeSize, 0.18)
-        TreeMiddle = BlzCreateFrame("ListBoxWar3", TalentMainFrame, 0, 1)
-        BlzFrameSetAbsPoint(TreeMiddle, FRAMEPOINT_TOPLEFT, middleTreeAlignment, 0.52)
-        BlzFrameSetAbsPoint(TreeMiddle, FRAMEPOINT_BOTTOMRIGHT, middleTreeAlignment + smallTreeSize, 0.26)
-        TreeRight = BlzCreateFrame("ListBoxWar3", TalentMainFrame, 0, 1)
-        BlzFrameSetAbsPoint(TreeRight, FRAMEPOINT_TOPLEFT, rightTreeAlignment, 0.52)
-        BlzFrameSetAbsPoint(TreeRight, FRAMEPOINT_BOTTOMRIGHT, rightTreeAlignment + treeSize, 0.18)
-
-        ConfirmTalentsButton = BlzCreateFrame("ScriptDialogButton", TalentMainFrame, 0, 0)
-        BlzFrameSetAbsPoint(ConfirmTalentsButton, FRAMEPOINT_TOPLEFT, leftTreeAlignment, 0.180410)
-        BlzFrameSetAbsPoint(ConfirmTalentsButton, FRAMEPOINT_BOTTOMRIGHT, leftTreeAlignment + 0.1, 0.151000)
-        BlzFrameSetText(ConfirmTalentsButton, "|cffFFFFFFConfirm|r")
-        BlzFrameSetScale(ConfirmTalentsButton, 1.00)
-
-        CancelTalentsButton = BlzCreateFrame("ScriptDialogButton", TalentMainFrame, 0, 0)
-        BlzFrameSetAbsPoint(CancelTalentsButton, FRAMEPOINT_TOPLEFT, rightTreeAlignment + treeSize - 0.1, 0.180410)
-        BlzFrameSetAbsPoint(CancelTalentsButton, FRAMEPOINT_BOTTOMRIGHT, rightTreeAlignment + treeSize, 0.151000)
-        BlzFrameSetText(CancelTalentsButton, "|cffFFFFFDUndo|r")
-        BlzFrameSetScale(CancelTalentsButton, 1.00)
-
-        CloseTalentViewButton = BlzCreateFrame("ScriptDialogButton", TalentMainFrame, 0, 0)
-        BlzFrameSetAbsPoint(CloseTalentViewButton, FRAMEPOINT_TOPLEFT, rightTreeAlignment + treeSize, 0.57)
-        BlzFrameSetAbsPoint(CloseTalentViewButton, FRAMEPOINT_BOTTOMRIGHT, rightTreeAlignment + treeSize + 0.035, 0.545)
-        BlzFrameSetText(CloseTalentViewButton, "|cffFCD20DX|r")
-        BlzFrameSetScale(CloseTalentViewButton, 1.00)
-
-        TitleBackgroundLeft = BlzCreateFrameByType("BACKDROP", "TitleBackgroundLeft", TreeLeft, "", 1)
-        BlzFrameSetAbsPoint(TitleBackgroundLeft, FRAMEPOINT_TOPLEFT, leftTreeAlignment, 0.54)
-        BlzFrameSetAbsPoint(TitleBackgroundLeft, FRAMEPOINT_BOTTOMRIGHT, leftTreeAlignment + treeSize, 0.513)
-        BlzFrameSetTexture(TitleBackgroundLeft, "UI/Glues/Loading/LoadBar/Loading-BarBorder.blp", 0, true)
-
-        TitleBackgroundMiddle = BlzCreateFrameByType("BACKDROP", "TitleBackgroundMiddle", TreeMiddle, "", 1)
-        BlzFrameSetAbsPoint(TitleBackgroundMiddle, FRAMEPOINT_TOPLEFT, middleTreeAlignment, 0.54)
-        BlzFrameSetAbsPoint(TitleBackgroundMiddle, FRAMEPOINT_BOTTOMRIGHT, middleTreeAlignment + smallTreeSize, 0.513)
-        BlzFrameSetTexture(TitleBackgroundMiddle, "UI/Glues/Loading/LoadBar/Loading-BarBorder.blp", 0, true)
-
-        TitleBackgroundRight = BlzCreateFrameByType("BACKDROP", "TitleBackgroundRight", TreeRight, "", 1)
-        BlzFrameSetAbsPoint(TitleBackgroundRight, FRAMEPOINT_TOPLEFT, rightTreeAlignment, 0.54)
-        BlzFrameSetAbsPoint(TitleBackgroundRight, FRAMEPOINT_BOTTOMRIGHT, rightTreeAlignment + treeSize, 0.513)
-        BlzFrameSetTexture(TitleBackgroundRight, "UI/Glues/Loading/LoadBar/Loading-BarBorder.blp", 0, true)
-
-        TitleLeft = BlzCreateFrameByType("TEXT", "", TitleBackgroundLeft, "", 0)
-        BlzFrameSetAbsPoint(TitleLeft, FRAMEPOINT_TOPLEFT, leftTreeAlignment, 0.53)
-        BlzFrameSetAbsPoint(TitleLeft, FRAMEPOINT_BOTTOMRIGHT, leftTreeAlignment + treeSize, 0.522)
-        BlzFrameSetText(TitleLeft, "Power")
-        BlzFrameSetEnable(TitleLeft, false)
-        BlzFrameSetScale(TitleLeft, 1.00)
-        BlzFrameSetTextAlignment(TitleLeft, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_MIDDLE)
-
-        TitleMiddle = BlzCreateFrameByType("TEXT", "", TitleBackgroundMiddle, "", 0)
-        BlzFrameSetAbsPoint(TitleMiddle, FRAMEPOINT_TOPLEFT, middleTreeAlignment, 0.53)
-        BlzFrameSetAbsPoint(TitleMiddle, FRAMEPOINT_BOTTOMRIGHT, middleTreeAlignment + smallTreeSize, 0.522)
-        BlzFrameSetText(TitleMiddle, "Economy")
-        BlzFrameSetEnable(TitleMiddle, false)
-        BlzFrameSetScale(TitleMiddle, 1.00)
-        BlzFrameSetTextAlignment(TitleMiddle, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_MIDDLE)
-
-        TitleRight = BlzCreateFrameByType("TEXT", "", TitleBackgroundRight, "", 0)
-        BlzFrameSetAbsPoint(TitleRight, FRAMEPOINT_TOPLEFT, rightTreeAlignment, 0.53)
-        BlzFrameSetAbsPoint(TitleRight, FRAMEPOINT_BOTTOMRIGHT, rightTreeAlignment + treeSize, 0.522)
-        BlzFrameSetText(TitleRight, "Utility")
-        BlzFrameSetEnable(TitleRight, false)
-        BlzFrameSetScale(TitleRight, 1.00)
-        BlzFrameSetTextAlignment(TitleRight, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_MIDDLE)
-
-        --prestige status
-        TreeStatus = BlzCreateFrame("ListBoxWar3", TalentMainFrame, 0, 1)
-        BlzFrameSetAbsPoint(TreeStatus, FRAMEPOINT_TOPLEFT, middleTreeAlignment, 0.24)
-        BlzFrameSetAbsPoint(TreeStatus, FRAMEPOINT_BOTTOMRIGHT, middleTreeAlignment + smallTreeSize, 0.18)
-
-        StatusPoints = BlzCreateFrameByType("TEXT", "name", TreeStatus, "", 0)
-        BlzFrameSetAllPoints(StatusPoints, TreeStatus)
-        BlzFrameSetText(StatusPoints, "")
-        BlzFrameSetEnable(StatusPoints, false)
-        BlzFrameSetScale(StatusPoints, 1.00)
-        BlzFrameSetTextAlignment(StatusPoints, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_MIDDLE)
-
-        TitleBackgroundStatus = BlzCreateFrameByType("BACKDROP", "", TreeMiddle, "", 1)
-        BlzFrameSetAbsPoint(TitleBackgroundStatus, FRAMEPOINT_TOPLEFT, middleTreeAlignment, 0.26)
-        BlzFrameSetAbsPoint(TitleBackgroundStatus, FRAMEPOINT_BOTTOMRIGHT, middleTreeAlignment + smallTreeSize, 0.233)
-        BlzFrameSetTexture(TitleBackgroundStatus, "UI/Glues/Loading/LoadBar/Loading-BarBorder.blp", 0, true)
-
-        TitleStatus = BlzCreateFrameByType("TEXT", "", TitleBackgroundStatus, "", 0)
-        BlzFrameSetAbsPoint(TitleStatus, FRAMEPOINT_TOPLEFT, middleTreeAlignment, 0.25)
-        BlzFrameSetAbsPoint(TitleStatus, FRAMEPOINT_BOTTOMRIGHT, middleTreeAlignment + smallTreeSize, 0.243)
-        BlzFrameSetText(TitleStatus, "|cffffcc00Prestige Level: 0")
-        BlzFrameSetEnable(TitleStatus, false)
-        BlzFrameSetScale(TitleStatus, 1.00)
-        BlzFrameSetTextAlignment(TitleStatus, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_MIDDLE)
-    end
-    --]]
-
     --cover health text
     HIDE_HEALTH_FRAME = BlzCreateFrameByType("BACKDROP", "", BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0), "", 0)
     BlzFrameSetAbsPoint(HIDE_HEALTH_FRAME, FRAMEPOINT_TOPLEFT, 0.225, 0.028)
@@ -408,16 +270,6 @@ OnInit.final("Frames", function(Require)
     BlzFrameSetAllPoints(SHIELD_TEXT, SHIELD_BACKDROP)
     BlzFrameSetTextAlignment(SHIELD_TEXT, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_CENTER)
     BlzFrameSetVisible(SHIELD_BACKDROP, false)
-
-    AFK_FRAME_BG = BlzCreateFrame("Leaderboard", BlzGetOriginFrame(ORIGIN_FRAME_WORLD_FRAME, 0), 0, 0)
-    AFK_FRAME = BlzCreateFrameByType("TEXT", "", AFK_FRAME_BG, "", 0)
-    BlzFrameSetText(AFK_FRAME, "")
-    BlzFrameSetTextAlignment(AFK_FRAME, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_CENTER)
-    BlzFrameSetPoint(AFK_FRAME, FRAMEPOINT_CENTER, BlzGetOriginFrame(ORIGIN_FRAME_WORLD_FRAME, 0), FRAMEPOINT_CENTER, 0, 0)
-
-    BlzFrameSetPoint(AFK_FRAME_BG, FRAMEPOINT_TOPLEFT, AFK_FRAME, FRAMEPOINT_TOPLEFT, -0.04, 0.03)
-    BlzFrameSetPoint(AFK_FRAME_BG, FRAMEPOINT_BOTTOMRIGHT, AFK_FRAME, FRAMEPOINT_BOTTOMRIGHT, 0.04, -0.03)
-    BlzFrameSetVisible(AFK_FRAME_BG, false)
 
     DPS_FRAME = BlzCreateFrameByType("BACKDROP", "", BlzGetOriginFrame(ORIGIN_FRAME_WORLD_FRAME, 0), "ButtonBackdropTemplate", 0)
     BlzFrameSetPoint(DPS_FRAME, FRAMEPOINT_CENTER, BlzGetOriginFrame(ORIGIN_FRAME_WORLD_FRAME, 0), FRAMEPOINT_TOP, 0.30, -0.37)
