b6494940f153a71f16af84d282294758ba010af0
toeneeoh <toeneeoh@gmail.com>
2025-05-23 23:52:34 -0400

    rework hero select to use custom UI

--- cotlua/src/Game/heroselect.lua
+++ cotlua/src/Game/heroselect.lua
@@ -4,290 +4,239 @@ OnInit.final("HeroSelect", function(Require)
     Require('Spells')
     Require('Frames')
 
-    local SELECT_DUMMY   = {} ---@type unit[] 
-    local SELECT_VIEWING = __jarray(0) ---@type integer[] 
-    local SELECT_STAT    = __jarray(0) ---@type integer[] 
-    local SELECT_SORT    = {} ---@type boolean[] 
-    local hardcore_clicked = {}
+    SELECTING_HERO = {} ---@type boolean[] 
+    local hardcore = {}
 
-    local unitMapping = {
-        [1] = FourCC('E001'),
-        [2] = FourCC('E004'),
-        [3] = FourCC('E01A')
-    }
+    -- sort all heroes alphabetically
+    local heroes = {}
 
-    ALICE_ExcludeTypes('E001', 'E004', 'E01A')
-
-    --- Create or replace the dummy unit based on the main stat index
-    --- @param pid integer
-    --- @param i integer
-    local function MainStatForm(pid, i)
-        RemoveUnit(SELECT_DUMMY[pid])
-        if unitMapping[i] then
-            SELECT_DUMMY[pid] = CreateUnit(Player(pid - 1), unitMapping[i], 30000, 30000, 0)
-        end
+    for i, v in pairs(HERO_STATS) do
+        -- automatically assign key to id value
+        v.id = i
+        -- get hero name
+        v.name = GetObjectName(i)
+        heroes[#heroes + 1] = v
     end
 
+    table.sort(heroes, function(a, b)
+        local name1 = string.lower(a.name)
+        local name2 = string.lower(b.name)
+        return name1 < name2
+    end)
+
+    --#region frame setup
+        local frame = BlzCreateFrameByType("BACKDROP", "", BlzGetFrameByName("ConsoleUIBackdrop", 0), "", 0)
+        BlzFrameSetAbsPoint(frame, FRAMEPOINT_TOP, 0.4, 0.53)
+        BlzFrameSetSize(frame, 0.7, 0.35)
+        BlzFrameSetEnable(frame, false)
+        BlzFrameSetTexture(frame, "war3mapImported\\HeroSelectionUI4.dds", 0, true)
+        BlzFrameSetVisible(frame, false)
+
+        local sprite_frame = BlzCreateFrameByType("SPRITE", "", frame, "", 0)
+        BlzFrameClearAllPoints(sprite_frame)
+        BlzFrameSetAbsPoint(sprite_frame, FRAMEPOINT_CENTER, 0.61, 0.285)
+        BlzFrameSetSize(sprite_frame, 0.001, 0.001)
+        BlzFrameSetScale(sprite_frame, 0.0008)
+
+        local select_button = SimpleButton.create(frame, "trans32.blp", 0.1, 0.033, FRAMEPOINT_BOTTOMRIGHT, FRAMEPOINT_BOTTOMRIGHT, -0.09, 0.03)
+        select_button:text("Select")
+        select_button:visible(false)
+        BlzFrameSetScale(select_button.text_frame, 1.5)
+
+        local name_label = BlzCreateFrame("TitleText", frame, 0, 0)
+        BlzFrameSetPoint(name_label, FRAMEPOINT_BOTTOM, select_button.frame, FRAMEPOINT_TOP, 0., 0.277)
+        BlzFrameSetScale(name_label, 0.85)
+
+        local stat_text1 = BlzCreateFrameByType("TEXT", "", frame, "", 0)
+        BlzFrameSetPoint(stat_text1, FRAMEPOINT_TOP, frame, FRAMEPOINT_TOP, 0.105, -0.06)
+        BlzFrameSetTextAlignment(stat_text1, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_LEFT)
+        BlzFrameSetSize(stat_text1, 0.3, 0.)
+        BlzFrameSetScale(stat_text1, 1.25)
+
+        local stat_text2 = BlzCreateFrameByType("TEXT", "", frame, "", 0)
+        BlzFrameSetPoint(stat_text2, FRAMEPOINT_TOP, frame, FRAMEPOINT_TOP, 0.18, -0.06)
+        BlzFrameSetTextAlignment(stat_text2, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_LEFT)
+        BlzFrameSetSize(stat_text2, 0.3, 0.)
+        BlzFrameSetScale(stat_text2, 1.25)
+
+        -- TODO: make a SimpleCheckbox class?
+        local hardcore_ticker = BlzCreateFrame("EscMenuCheckBoxTemplate", frame, 0, 0)
+        BlzFrameSetPoint(hardcore_ticker, FRAMEPOINT_BOTTOM, select_button.frame, FRAMEPOINT_TOP, 0., 0.01)
+        local hardcore_icon = SimpleButton.create(hardcore_ticker, "ReplaceableTextures\\CommandButtons\\BTNHardcore.blp", 0.02, 0.02, FRAMEPOINT_RIGHT, FRAMEPOINT_LEFT, 0., 0.)
+        hardcore_icon:makeTooltip(FRAMEPOINT_TOPRIGHT, 0.2)
+        hardcore_icon:setTooltipIcon("ReplaceableTextures\\CommandButtons\\BTNHardcore.blp")
+        hardcore_icon:setTooltipName("Hardcore Mode")
+        hardcore_icon:setTooltipText("If your character is hardcore blablabla we still don't know what the fuck hardcore does pepeSmilers")
+        hardcore_icon:enable(false)
+        local t = CreateTrigger()
+
+        BlzTriggerRegisterFrameEvent(t, hardcore_ticker, FRAMEEVENT_CHECKBOX_CHECKED)
+        BlzTriggerRegisterFrameEvent(t, hardcore_ticker, FRAMEEVENT_CHECKBOX_UNCHECKED)
+
+        TriggerAddAction(t, function()
+            local f = BlzGetTriggerFrame()
+            local p = GetTriggerPlayer()
+            local pid = GetPlayerId(p) + 1
+
+            hardcore[pid] = not hardcore[pid]
+
+            if GetLocalPlayer() == p then
+                BlzFrameSetEnable(f, false)
+                BlzFrameSetEnable(f, true)
+                hardcore_icon:enable(hardcore[pid])
+            end
+        end)
+
+        local icon_x_gap = 0.0385
+        local icon_y_gap = 0.04
+
+        local hero_buttons = {}
+        local agi_count = 0
+        local str_count = 0
+        local int_count = 0
+
+        -- hero buttons
+        for i, v in ipairs(heroes) do
+            local count = 0
+            local x_gap = 0.05
+            local y_gap = 0.01
+            if v.main == "str" then
+                count = str_count
+                str_count = str_count + 1
+                y_gap = 0.053
+            elseif v.main == "agi" then
+                count = agi_count
+                agi_count = agi_count + 1
+                y_gap = 0.152
+            elseif v.main == "int" then
+                count = int_count
+                int_count = int_count + 1
+                y_gap = 0.25
+            end
+            local x = math.fmod(count, 5) * icon_x_gap + x_gap
+            local y = (count // 5) * icon_y_gap + y_gap
+            hero_buttons[i] = SimpleButton.create(frame, BlzGetAbilityIcon(v.id), 0.028, 0.028, FRAMEPOINT_TOPLEFT, FRAMEPOINT_TOPLEFT, x, -y)
+            hero_buttons[i]:makeTooltip(FRAMEPOINT_TOPLEFT, 0.2)
+            hero_buttons[i]:setTooltipIcon(BlzGetAbilityIcon(v.id))
+            hero_buttons[i]:setTooltipName(v.name)
+            hero_buttons[i]:setTooltipText(BlzGetAbilityExtendedTooltip(v.select, 0))
+        end
+
+        local selected = {}
+
+        local on_confirm = function()
+            local f = BlzGetTriggerFrame()
+            local p = GetTriggerPlayer()
+            local pid = GetPlayerId(p) + 1
+
+            if GetLocalPlayer() == p then
+                BlzFrameSetEnable(f, false)
+                BlzFrameSetEnable(f, true)
+            end
+
+            if selected[pid] and SELECTING_HERO[pid] then
+                SelectHero(pid, heroes[selected[pid]].id)
+            end
+        end
+
+        select_button:onClick(on_confirm)
+
+        -- ability icons
+        local abilities = {}
+
+        for i = 1, 6 do
+            local x = math.fmod(i - 1, 3)
+            local y = (i - 1) // 3
+            abilities[i] = SimpleButton.create(frame, "", 0.028, 0.028, FRAMEPOINT_BOTTOM, FRAMEPOINT_BOTTOM, -0.0638 + x * 0.0385, 0.076 - y * 0.0395)
+            abilities[i]:makeTooltip(FRAMEPOINT_TOPRIGHT, 0.2)
+            abilities[i]:visible(false)
+        end
+
+        -- info icon
+        local info = SimpleButton.create(frame, "", 0.028, 0.028, FRAMEPOINT_BOTTOM, FRAMEPOINT_BOTTOM, 0.0637, 0.0563)
+        info:makeTooltip(FRAMEPOINT_TOPRIGHT, 0.2)
+        info:visible(false)
+
+        local on_click = function()
+            local f = BlzGetTriggerFrame()
+            local p = GetTriggerPlayer()
+            local pid = GetPlayerId(p) + 1
+
+            if GetLocalPlayer() == p then
+                BlzFrameSetEnable(f, false)
+                BlzFrameSetEnable(f, true)
+            end
+
+            for i, v in ipairs(hero_buttons) do
+                if f == v.frame then
+                    local hero = heroes[i]
+                    selected[pid] = i
+                    if GetLocalPlayer() == p then
+                        select_button:visible(true)
+                        info:visible(true)
+                        info:icon(BlzGetAbilityIcon(hero.passive))
+                        info:setTooltipIcon(BlzGetAbilityIcon(hero.passive))
+                        info:setTooltipName(GetAbilityName(hero.passive))
+                        info:setTooltipText(BlzGetAbilityExtendedTooltip(hero.passive, 0))
+                        BlzFrameSetText(stat_text1, hero.str .. " (+" .. (hero.str_gain or 0) .. ")\n\n\n" .. hero.agi .. " (+" .. (hero.agi_gain or 0) .. ")\n\n\n" .. hero.int .. " (+" .. (hero.int_gain or 0) .. ")\n\n\n")
+                        BlzFrameSetText(stat_text2, (hero[hero.main] + 1) .. "\n\n\n" .. hero.armor .. "\n\n\n" .. hero.range .. "\n\n\n")
+                        BlzFrameSetText(name_label, hero.name)
+                        BlzFrameSetModel(sprite_frame, hero.model, 1)
+                        BlzFrameSetSpriteAnimate(sprite_frame, 2, 0)
+                    end
+
+                    for j = 1, 6 do
+                        if hero.skills[j] then
+                            local abil = FourCC(hero.skills[j])
+                            if GetLocalPlayer() == p then
+                                abilities[j]:visible(true)
+                                abilities[j]:icon(BlzGetAbilityIcon(abil))
+                                abilities[j]:setTooltipIcon(BlzGetAbilityIcon(abil))
+                                abilities[j]:setTooltipName(GetAbilityName(abil))
+                                abilities[j]:setTooltipText(Spells[abil]:getTooltip())
+                            end
+                        else
+                            if GetLocalPlayer() == p then
+                                abilities[j]:visible(false)
+                            end
+                        end
+                    end
+
+                    break
+                end
+            end
+        end
+
+        for _, v in ipairs(hero_buttons) do
+            v:onClick(on_click)
+        end
+    --#endregion
+
     ---@param pid integer
-    function StartGame(pid)
+    ---@param id integer
+    function SelectHero(pid, id)
         local p = Player(pid - 1)
 
-        PauseUnit(Hero[pid], false)
+        Profile[pid]:new_character(id)
+        Profile[pid].hero.hardcore = (hardcore[pid] and 1) or 0
+        SELECTING_HERO[pid] = false
 
         if (GetLocalPlayer() == p) then
+            ClearTextMessages()
             ClearSelection()
             SelectUnit(Hero[pid], true)
             ResetToGameCamera(0)
+            BlzFrameSetVisible(frame, false)
         end
 
         ExperienceControl(pid)
         CharacterSetup(pid, false)
     end
 
-    ---@param pid integer
-    local function LockCycle(pid)
-        if SELECT_VIEWING[pid] > HERO_TOTAL - 1 then
-            SELECT_VIEWING[pid] = 0
-        elseif SELECT_VIEWING[pid] < 0 then
-            SELECT_VIEWING[pid] = HERO_TOTAL - 1
-        end
-    end
-
-    ---@param pid integer
-    ---@param direction integer
-    local function Scroll(pid, direction)
-        UnitRemoveAbility(SELECT_DUMMY[pid], HeroCircle[SELECT_VIEWING[pid]].select)
-        UnitRemoveAbility(SELECT_DUMMY[pid], HeroCircle[SELECT_VIEWING[pid]].passive)
-
-        repeat
-            SELECT_VIEWING[pid] = SELECT_VIEWING[pid] + direction
-            LockCycle(pid)
-        until not (SELECT_SORT[pid] and (MainStat(HeroCircle[SELECT_VIEWING[pid]].unit) ~= SELECT_STAT[pid]))
-
-        local current_hero = HeroCircle[SELECT_VIEWING[pid]]
-
-        MainStatForm(pid, MainStat(current_hero.unit))
-
-        local selected_unit = SELECT_DUMMY[pid]
-
-        BlzSetUnitSkin(selected_unit, current_hero.skin)
-        BlzSetUnitName(selected_unit, GetUnitName(current_hero.unit))
-        BlzSetHeroProperName(selected_unit, GetHeroProperName(current_hero.unit))
-        BlzSetUnitWeaponIntegerField(selected_unit, UNIT_WEAPON_IF_ATTACK_ATTACK_TYPE, 0, BlzGetUnitWeaponIntegerField(current_hero.unit, UNIT_WEAPON_IF_ATTACK_ATTACK_TYPE, 0))
-        BlzSetUnitIntegerField(selected_unit, UNIT_IF_DEFENSE_TYPE, BlzGetUnitIntegerField(current_hero.unit, UNIT_IF_DEFENSE_TYPE))
-        BlzSetUnitWeaponRealField(selected_unit, UNIT_WEAPON_RF_ATTACK_BASE_COOLDOWN, 0, BlzGetUnitWeaponRealField(current_hero.unit, UNIT_WEAPON_RF_ATTACK_BASE_COOLDOWN, 0))
-        BlzSetUnitWeaponRealField(selected_unit, UNIT_WEAPON_RF_ATTACK_RANGE, 1, BlzGetUnitWeaponRealField(current_hero.unit, UNIT_WEAPON_RF_ATTACK_RANGE, 0) - 100)
-        BlzSetUnitArmor(selected_unit, BlzGetUnitArmor(current_hero.unit))
-
-        Unit[selected_unit].str = Unit[current_hero.unit].str
-        Unit[selected_unit].agi = Unit[current_hero.unit].agi
-        Unit[selected_unit].int = Unit[current_hero.unit].int
-
-        BlzSetUnitBaseDamage(selected_unit, BlzGetUnitBaseDamage(current_hero.unit, 0), 0)
-        BlzSetUnitDiceNumber(selected_unit, BlzGetUnitDiceNumber(current_hero.unit, 0), 0)
-        BlzSetUnitDiceSides(selected_unit, BlzGetUnitDiceSides(current_hero.unit, 0), 0)
-
-        -- BlzSetUnitMaxHP(selected_unit, BlzGetUnitMaxHP(current_hero.unit))
-        -- BlzSetUnitMaxMana(selected_unit, BlzGetUnitMaxMana(current_hero.unit))
-        SetWidgetLife(selected_unit, BlzGetUnitMaxHP(selected_unit))
-
-        UnitAddAbility(selected_unit, current_hero.select)
-        UnitAddAbility(selected_unit, current_hero.passive)
-
-        UnitAddAbility(selected_unit, FourCC('A0JI'))
-        UnitAddAbility(selected_unit, FourCC('A0JQ'))
-        UnitAddAbility(selected_unit, FourCC('A0JR'))
-        UnitAddAbility(selected_unit, FourCC('A0JS'))
-        UnitAddAbility(selected_unit, FourCC('A0JT'))
-        UnitAddAbility(selected_unit, FourCC('A0JU'))
-        UnitAddAbility(selected_unit, FourCC('Aeth'))
-        SetUnitPathing(selected_unit, false)
-        UnitRemoveAbility(selected_unit, FourCC('Amov'))
-        BlzUnitHideAbility(selected_unit, FourCC('Aatk'), true)
-
-        if (GetLocalPlayer() == Player(pid - 1)) then
-            -- move passive Y position to (0,1)
-            BlzSetAbilityPosY(current_hero.passive, 1)
-            SetCameraField(CAMERA_FIELD_TARGET_DISTANCE, 500, 0)
-            SetCameraField(CAMERA_FIELD_ANGLE_OF_ATTACK, 340, 0)
-            SetCameraField(CAMERA_FIELD_FIELD_OF_VIEW, 60, 0)
-            SetCameraField(CAMERA_FIELD_ZOFFSET, 200, 0)
-            SetCameraField(CAMERA_FIELD_ROTATION, GetUnitFacing(current_hero.unit) + 180, 0)
-            SetCameraTargetController(gg_unit_h00T_0511, 0, 0, false)
-            ClearSelection()
-            SelectUnit(selected_unit, true)
-        end
-    end
-
-    ---@param pid integer
     function StartHeroSelect(pid)
-        SELECT_VIEWING[pid] = HERO_TOTAL - 1
-        Scroll(pid, 1)
-    end
-
-    ---@return boolean
-    function HardcoreMenu()
-        local pid = GetPlayerId(GetTriggerPlayer()) + 1 ---@type integer 
-
-        if not hardcore_clicked[pid] then
-            hardcore_clicked[pid] = true
-            return true
-        end
-
-        return false
-    end
-
-    function HardcoreYes()
-        local pid = GetPlayerId(GetTriggerPlayer()) + 1 ---@type integer 
-
-        Profile[pid].hero.hardcore = 1
-
-        StartGame(pid)
-
-        if GetLocalPlayer() == GetTriggerPlayer() then
-            BlzFrameSetEnable(BlzGetTriggerFrame(), false)
-            BlzFrameSetEnable(BlzGetTriggerFrame(), true)
-            BlzFrameSetVisible(HARDCORE_BACKDROP, false)
-        end
-    end
-
-    function HardcoreNo()
-        local pid = GetPlayerId(GetTriggerPlayer()) + 1 ---@type integer 
-        StartGame(pid)
-
-        if GetLocalPlayer() == GetTriggerPlayer() then
-            BlzFrameSetEnable(BlzGetTriggerFrame(), false)
-            BlzFrameSetEnable(BlzGetTriggerFrame(), true)
-            BlzFrameSetVisible(HARDCORE_BACKDROP, false)
-        end
-    end
-
-    ---@param pid integer
-    ---@param id integer
-    function Selection(pid, id)
-        local p = Player(pid - 1)
-
-        RemoveUnit(SELECT_DUMMY[pid])
-
-        Profile[pid]:new_character(id)
-        SELECTING_HERO[pid] = false
-        hardcore_clicked[pid] = false
-
-        if (GetLocalPlayer() == p) then
+        if GetLocalPlayer() == Player(pid - 1) then
+            BlzFrameSetVisible(frame, true)
             ClearTextMessages()
-            BlzFrameSetVisible(HARDCORE_BACKDROP, true)
         end
     end
-
-    ---@return boolean
-    local function IsSelecting()
-        local pid = GetPlayerId(GetTriggerPlayer()) + 1
-
-        if SELECTING_HERO[pid] then
-            if BlzGetTriggerPlayerKey() == OSKEY_LEFT then
-                Scroll(pid, -1)
-            else
-                Scroll(pid, 1)
-            end
-        end
-
-        return false
-    end
-
-    local CYCLE_LEFT = Spell.define('A0JI')
-    do
-        local thistype = CYCLE_LEFT
-
-        function thistype:onCast()
-            Scroll(self.pid, -1)
-        end
-    end
-
-    local CYCLE_RIGHT = Spell.define('A0JQ')
-    do
-        local thistype = CYCLE_RIGHT
-
-        function thistype:onCast()
-            Scroll(self.pid, 1)
-        end
-    end
-
-    local SORT_AGI = Spell.define('A0JR')
-    do
-        local thistype = SORT_AGI
-
-        function thistype:onCast()
-            SELECT_SORT[self.pid] = true
-            SELECT_STAT[self.pid] = 3
-            Scroll(self.pid, 1)
-        end
-    end
-
-    local SORT_STR = Spell.define('A0JS')
-    do
-        local thistype = SORT_STR
-
-        function thistype:onCast()
-            SELECT_SORT[self.pid] = true
-            SELECT_STAT[self.pid] = 1
-            Scroll(self.pid, 1)
-        end
-    end
-
-    local SORT_INT = Spell.define('A0JT')
-    do
-        local thistype = SORT_INT
-
-        function thistype:onCast()
-            SELECT_SORT[self.pid] = true
-            SELECT_STAT[self.pid] = 2
-            Scroll(self.pid, 1)
-        end
-    end
-
-    local STOP_SORT = Spell.define('A0JU')
-    do
-        local thistype = STOP_SORT
-
-        function thistype:onCast()
-            SELECT_SORT[self.pid] = false
-        end
-    end
-
-    local SELECT_HERO = Spell.define('A07S', 'A07T', 'A07U', 'A07V', 'A029', 'A07W', 'A07Z', 'A080', 'A081', 'A082', 'A084', 'A086', 'A087', 'A089', 'A07J', 'A01P', 'A07L', 'A07M', 'A07N')
-    do
-        local thistype = SELECT_HERO
-
-        function thistype:onCast()
-            Selection(self.pid, HeroCircle[SELECT_VIEWING[self.pid]].skin)
-        end
-    end
-
-    local arrow = CreateTrigger()
-    local u = User.first ---@type User 
-
-    --[[meta keys:
-    none: 0
-    shift: 1
-    ctrl: 2
-    alt: 4
-    windows key: 8]]
-    while u do
-        BlzTriggerRegisterPlayerKeyEvent(arrow, u.player, OSKEY_LEFT, 0, true)
-        BlzTriggerRegisterPlayerKeyEvent(arrow, u.player, OSKEY_RIGHT, 0, true)
-        u = u.next
-    end
-
-    local hardcoreSelectYes = CreateTrigger()
-    local hardcoreSelectNo = CreateTrigger()
-
-    TriggerAddCondition(hardcoreSelectYes, Condition(HardcoreMenu))
-    TriggerAddAction(hardcoreSelectYes, HardcoreYes)
-    BlzTriggerRegisterFrameEvent(hardcoreSelectYes, HARDCORE_BUTTON_FRAME, FRAMEEVENT_CONTROL_CLICK)
-
-    TriggerAddCondition(hardcoreSelectNo, Condition(HardcoreMenu))
-    TriggerAddAction(hardcoreSelectNo, HardcoreNo)
-    BlzTriggerRegisterFrameEvent(hardcoreSelectNo, HARDCORE_BUTTON_FRAME2, FRAMEEVENT_CONTROL_CLICK)
-
-    TriggerAddCondition(arrow, Condition(IsSelecting))
 end, Debug and Debug.getLine())
